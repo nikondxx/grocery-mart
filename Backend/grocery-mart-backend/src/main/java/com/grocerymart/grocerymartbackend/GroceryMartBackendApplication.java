690884d1639476b43fcbf905672efed2618e4624
nikondxx <nikondx@protonmail.com>
2025-06-29 11:08:57 +0530

    Initial Backend steup

---
+++ Backend/grocery-mart-backend/src/main/java/com/grocerymart/grocerymartbackend/GroceryMartBackendApplication.java
@@ -0,0 +1,13 @@
+package com.grocerymart.grocerymartbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroceryMartBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GroceryMartBackendApplication.class, args);
+    }
+
+}
---
+++ Backend/grocery-mart-backend/src/test/java/com/grocerymart/grocerymartbackend/GroceryMartBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grocerymart.grocerymartbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroceryMartBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
